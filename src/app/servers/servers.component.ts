bfbd2cba3b21fd248df723cb703d01bd021a90a4
Leonidas Yopan <leonidas@prevision.com.br>
2021-04-28 18:47:35 -0300

    display name of server along the message

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -20,7 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was successfully created!";
+    this.serverCreationStatus = "Server was successfully created! Server name is " + this.serverName + ".";
   }
 
   onUpdateServerName(event: Event) {
